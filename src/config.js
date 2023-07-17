6206f99ac550396046f3e54bba0bcad17e5a0748
Sumaya <sumaya.mohamedahmed@student.ap.be>
2023-07-17 17:20:28 +0200

    Testing if the api endpoint is working

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ export class Config{
     static get ApolloClient(){
        return new ApolloClient({
         cache: new InMemoryCache(),
-        uri: `${process.env.REACT_APP_GRAPHQL_API_URL}` 
+        uri: `${'https://admin.tavernescheldeland.be/graphql'}` 
        })
     }
 }
