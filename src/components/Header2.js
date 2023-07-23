b4cc9da4fb1ff2e63264c830e64683a4aeee5930
Sumaya <sumaya.mohamedahmed@student.ap.be>
2023-07-23 17:06:25 +0200

    Fixed the scroll position

--- src/components/Header2.js
+++ src/components/Header2.js
@@ -8,23 +8,29 @@ import { NavLink } from "react-router-dom"
 const Header2 = () => {
     const [toggled, setToggled] = useState(false);
     const navRef = useRef();
+
     const showNavbar = () => {
         setToggled(!toggled)
     }
+
+    const handleClick = ()=> {
+        window.scrollTo({ top: 0 });
+      }
+      
     return (
         <header>
             <a href="/" className={style.logoContainer}><img src="/logo.png" className={style.logo}/></a>
             <nav ref={navRef} className={toggled ? style.responsive_nav : ""}>
-                <NavLink to="/" className={({ isActive }) => (isActive ? `${style.active} ${style.navlink}` : `${style.inactive}`)} >
+                <NavLink to="/" className={({ isActive }) => (isActive ? `${style.active} ${style.navlink}` : `${style.inactive}`)} onClick={handleClick}>
                     Home
                 </NavLink>
-                <NavLink to="/about" className={({ isActive }) => (isActive ? `${style.active} ${style.navlink}` : `${style.inactive}`)} >
+                <NavLink to="/about" className={({ isActive }) => (isActive ? `${style.active} ${style.navlink}` : `${style.inactive}`)} onClick={handleClick}>
                     Over ons
                 </NavLink>
-                <NavLink to="/contact" className={({ isActive }) => (isActive ? `${style.active} ${style.navlink}` : `${style.inactive}`)} >
+                <NavLink to="/contact" className={({ isActive }) => (isActive ? `${style.active} ${style.navlink}` : `${style.inactive}`)} onClick={handleClick}>
                     Contact
                 </NavLink>
-                <NavLink to="/menu" className={({ isActive }) => (isActive ? `${style.active} ${style.navlink}` : `${style.inactive}`)} >
+                <NavLink to="/menu" className={({ isActive }) => (isActive ? `${style.active} ${style.navlink}` : `${style.inactive}`)} onClick={handleClick}>
                     Menu
                 </NavLink>
                 <button className={`${style.nav_btn} ${style.nav_close_btn}`} onClick={showNavbar}>
