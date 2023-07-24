5e97751847fb39b13fe2b794b6f5705e35219ee1
Sumaya <sumaya.mohamedahmed@student.ap.be>
2023-07-24 10:15:42 +0200

    the background image margin changes

--- src/dishes/Dish.js
+++ src/dishes/Dish.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate, NavLink, Link } from 'react-router-dom'
 import { useGetDish } from './_query';
 import Layout from '../components/layout'
 import styles from "./dish.module.css"
 import { Spinner } from '../shared/Spinner'
+import backgroungImage from "./../images/food-background3.jpg" 
 
 export const Dish = () => {
   const { data, isLoading } = useGetDish()
   const navigate = useNavigate();
+  const  [stateBackgroungImage, setStateBackgroungImage]  = useState("");
 
   const goBack = ()=>{
     navigate(-1)
   }
   
+  useEffect(()=>{
+    setStateBackgroungImage(backgroungImage)
+  },[backgroungImage]);
+
   if (isLoading) return <Spinner />
   return (
     <Layout>
-      <div className={styles.container}>
+      <div className={styles.container} style={{backgroundImage: `url(${stateBackgroungImage})`  }}>
         <div className={styles.miniContainer}>
           <h2 className={styles.title}>{data?.title}</h2>
           <div className={styles.imageContainer}><img src={data?.profilePicture.sourceUrl} alt={data?.profilePicture.altText} className={styles.pic} /></div>
