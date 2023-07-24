cb4a5a3a1504efba2b72de8b00099a7a1a8ef0c4
Sumaya <sumaya.mohamedahmed@student.ap.be>
2023-07-24 11:14:06 +0200

    Iframe adjustment

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -10,50 +10,51 @@ export const Contact = () => {
   if (isLoading) return <Spinner />
   return (
     <Layout>
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2977.8498601498263!2d4.285960943380119!3d51.12574092945093!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3f2ed903fa9b9%3A0xa6f5bc488e105b17!2sScheldeland!5e0!3m2!1snl!2sbe!4v1670838055770!5m2!1snl!2sbe" style={{ width: "100%", height: "38vh", style: "border:0;" }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       <div className={styles.contactContainer}>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2977.8498601498263!2d4.285960943380119!3d51.12574092945093!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3f2ed903fa9b9%3A0xa6f5bc488e105b17!2sScheldeland!5e0!3m2!1snl!2sbe!4v1670838055770!5m2!1snl!2sbe" style={{ width: "100%", height: "38vh" }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" className={styles.iframe}></iframe>
+
         <div className={styles.smallContainer}>
 
           {/* <h2>{data?.title}</h2> */}
 
 
           <div className={styles.adressContainer}>
-          <h2>Contacteer ons</h2>
-     <div className={styles.adressMiniContainer}>
-     <div className={styles.address}>
-              <img style={{ width: "15%", height: "15%" }} src='./location2.png' />
-              <a href="https://www.google.com/maps?t=m&hl=nl&gl=BE&cid=12030728999397251863" target="_blank">
-                <div> {data?.address}</div>
-                <div> {data?.city}</div>
-              </a>
+            <h2>Contacteer ons</h2>
+            <div className={styles.adressMiniContainer}>
+              <div className={styles.address}>
+                <img style={{ width: "15%", height: "15%" }} src='./location2.png' />
+                <a href="https://www.google.com/maps?t=m&hl=nl&gl=BE&cid=12030728999397251863" target="_blank">
+                  <div> {data?.address}</div>
+                  <div> {data?.city}</div>
+                </a>
 
-              {/* <a href="https://www.google.com/maps?t=m&hl=nl&gl=BE&cid=12030728999397251863" target="_blank">
+                {/* <a href="https://www.google.com/maps?t=m&hl=nl&gl=BE&cid=12030728999397251863" target="_blank">
              </a> */}
 
-            </div>
+              </div>
 
-            <div className={styles.phone}>
-              <img style={{ width: "15%", height: "15%" }} src='./gsm3.png' />
-              <a href="tel:+4733378901" >{data?.phoneNumber}</a>
-            </div>
+              <div className={styles.phone}>
+                <img style={{ width: "15%", height: "15%" }} src='./gsm3.png' />
+                <a href="tel:+4733378901" >{data?.phoneNumber}</a>
+              </div>
 
-            <div className={styles.email}>
-              <img style={{ width: "15%", height: "15%" }} src='./email2.png' />
-              <a href="mailto:info@tavernescheldeland.be" >{data?.email}</a>
+              <div className={styles.email}>
+                <img style={{ width: "15%", height: "15%" }} src='./email2.png' />
+                <a href="mailto:info@tavernescheldeland.be" >{data?.email}</a>
+              </div>
             </div>
-     </div>
 
           </div>
 
-<div className={styles.openingHours}>
-<h2>Openingsuren</h2>
-<div className={styles.daysAndHours}><span className={styles.days}>{data?.openingHours.wednesday.day}</span> <span className={styles.hours}>{data?.openingHours.wednesday.openingTime} - {data?.openingHours.wednesday.closingTime}</span></div>
-<div className={styles.daysAndHours}><span className={styles.days}>{data?.openingHours.thursday.day}</span> <span className={styles.hours}>{data?.openingHours.thursday.openingTime} - {data?.openingHours.thursday.closingTime}</span></div>
-<div className={styles.daysAndHours}><span className={styles.days}>{data?.openingHours.friday.day}</span> <span className={styles.hours}>{data?.openingHours.friday.openingTime} - {data?.openingHours.friday.closingTime}</span></div>
-<div className={styles.daysAndHours}><span className={styles.days}>{data?.openingHours.saturday.day}</span> <span className={styles.hours}>{data?.openingHours.saturday.openingTime} - {data?.openingHours.saturday.closingTime}</span></div>
-<div className={styles.daysAndHours}><span className={styles.days}>{data?.openingHours.sunday.day}</span> <span className={styles.hours}>{data?.openingHours.sunday.openingTime} - {data?.openingHours.sunday.closingTime}</span></div>
+          <div className={styles.openingHours}>
+            <h2>Openingsuren</h2>
+            <div className={styles.daysAndHours}><span className={styles.days}>{data?.openingHours.wednesday.day}</span> <span className={styles.hours}>{data?.openingHours.wednesday.openingTime} - {data?.openingHours.wednesday.closingTime}</span></div>
+            <div className={styles.daysAndHours}><span className={styles.days}>{data?.openingHours.thursday.day}</span> <span className={styles.hours}>{data?.openingHours.thursday.openingTime} - {data?.openingHours.thursday.closingTime}</span></div>
+            <div className={styles.daysAndHours}><span className={styles.days}>{data?.openingHours.friday.day}</span> <span className={styles.hours}>{data?.openingHours.friday.openingTime} - {data?.openingHours.friday.closingTime}</span></div>
+            <div className={styles.daysAndHours}><span className={styles.days}>{data?.openingHours.saturday.day}</span> <span className={styles.hours}>{data?.openingHours.saturday.openingTime} - {data?.openingHours.saturday.closingTime}</span></div>
+            <div className={styles.daysAndHours}><span className={styles.days}>{data?.openingHours.sunday.day}</span> <span className={styles.hours}>{data?.openingHours.sunday.openingTime} - {data?.openingHours.sunday.closingTime}</span></div>
 
-</div>
+          </div>
         </div>
       </div>
     </Layout>
