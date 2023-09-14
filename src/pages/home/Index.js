7c42ec6d6866ebe6f687728fca2ed0c8158ec991
Sumaya <sumaya.mohamedahmed@student.ap.be>
2023-09-14 19:16:34 +0200

    Testing for mobile parrallax

--- src/pages/home/Index.js
+++ src/pages/home/Index.js
@@ -14,52 +14,46 @@ export const Home = () => {
   return (
     <Layout>
       <div className={styles.container}>
-        <div className={`${styles.mainImageContainer} ${styles.parallax}`} style={{ backgroundImage: `url(${data?.mainimage?.sourceUrl})` }}>
-          {/* <img src={data?.mainimage?.sourceUrl} alt={data?.mainimage?.altText} className={styles.mainImage} /> */}
-          {/* </div> */}
 
-          {/* <div className={`${styles.secondSectionContainer} ${styles.parallax}`}> */}
+        <div className={styles.parallaxContainer}>  
+          <div className={`${styles.mainImageContainer} ${styles.parallax}`} style={{ backgroundImage: `url(${data?.mainimage?.sourceUrl})` }}>
+            <div className={styles.descriptionContainer}>
+              <h2 className={`${styles.title} ${styles.text}`} >{data?.title}</h2>
+              <div className={styles.description}>{data?.description}</div>
+            </div>
 
-          {/* <div className={styles.secondSectionImageContainer}>
-       <img src='./taverne.jpg' className={styles.secondSectionImage}/>
+          </div>
+       </div>
+   
 
-       </div> */}
-          <div className={styles.descriptionContainer}>
-            <h2 className={`${styles.title} ${styles.text}`} >{data?.title}</h2>
-            <div className={styles.description}>{data?.description}</div>
+        <div className={styles.contentOuter}>
+          <div className={styles.containerImage}> <h1 className={`${styles.topTitle}`}>Populaire gerechten</h1>     </div>
+
+          <Carousel items={data && data?.gerechten?.map((item) => item.gerechtmeta)} />
+
+          <div className={styles.middleContainer}>
+            <div className={`${styles.cardContainer}`}>
+
+              {data?.gerechten?.map((item) =>
+                <div key={item.slug} className={styles.card}>
+                  <NavLink to={`${"menu"}/${item.slug}`} className={styles.link}>
+                    <img src={item.gerechtmeta?.menuimage?.sourceUrl} className={styles.dishesImage} />
+                    <div className={styles.dishesTitle}>{item.gerechtmeta?.title}</div>
+                    <p className={styles.price}>{item.gerechtmeta?.price}</p>
+                  </NavLink>
+                </div>
+              )}
+            </div>
           </div>
 
-        </div>
 
 
+          {/* <div className={styles.containerSharingFoodImage}><h1 className={styles.sharingTopTitle}>Top 3 Sharing food</h1></div>   */}
 
-        <div className={styles.containerImage}> <h1 className={`${styles.topTitle}`}>Populaire gerechten</h1>     </div>
-
-        <Carousel items={data && data?.gerechten?.map((item) => item.gerechtmeta)} />
-
-        <div className={styles.middleContainer}>
-          <div className={`${styles.cardContainer}`}>
-         
-            {data?.gerechten?.map((item) =>
-              <div key={item.slug} className={styles.card}>
-                <NavLink to={`${"menu"}/${item.slug}`} className={styles.link}>
-                  <img src={item.gerechtmeta?.menuimage?.sourceUrl} className={styles.dishesImage} />
-                  <div className={styles.dishesTitle}>{item.gerechtmeta?.title}</div>
-                  <p className={styles.price}>{item.gerechtmeta?.price}</p>
-                </NavLink>
-              </div>
-            )}
-          </div>
-        </div>
+          {/* <Carousel items={data && data?.sharingfood?.map((item)=> item.sharingFoodmeta)}  /> */}
 
 
-
-        {/* <div className={styles.containerSharingFoodImage}><h1 className={styles.sharingTopTitle}>Top 3 Sharing food</h1></div>   */}
-
-        {/* <Carousel items={data && data?.sharingfood?.map((item)=> item.sharingFoodmeta)}  /> */}
-
-
-        {/* 
+          {/* 
         
         <div className={styles.middleContainer}>
         <div className={styles.cardContainer}>
@@ -76,6 +70,7 @@ export const Home = () => {
         </div> 
                 </div>*/}
 
+        </div>
 
 
       </div>
