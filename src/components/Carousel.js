6362999d70bf92ebe9680dc2aeb2f0f259499ab7
Sumaya <sumaya.mohamedahmed@student.ap.be>
2024-01-26 12:16:19 +0100

    iphone problem second time

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -19,8 +19,11 @@ export const Carousel = ({ items }) => {
             <BsArrowLeftCircleFill className={`${styles.arrow}  ${styles.arrowLeft}`} onClick={nextSlide}/>
             {data?.map((item, index) =>
                 <div  className={styles.contentContainer}>
-                    <img src={item?.menuimage?.sourceUrl} className={slide === index ? `${styles.slide}` : `${styles.slide} ${styles.slideHidden}`}/>
-                    <div className={slide === index ? `${styles.title}` : `${styles.title} ${styles.titleHidden}`}>
+            <div className={styles.imageContainer}>
+            <img src={item?.menuimage?.sourceUrl} className={slide === index ? `${styles.slide}` : `${styles.slide} ${styles.slideHidden}`}/>
+
+                </div>                    
+                <div className={slide === index ? `${styles.title}` : `${styles.title} ${styles.titleHidden}`}>
                         <p>{item?.title}</p>
                         <p className={styles.price}>{item?.price}</p>
                         </div>                   
