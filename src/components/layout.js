7af106174124cb7387c9c698e85a92243453ec6d
Sumaya <sumaya.mohamedahmed@student.ap.be>
2024-03-09 12:40:37 +0100

    Testing the scrolling problem5

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,11 +5,10 @@ import Header from "./Header"
 import Footer from "./footer"
 
 const Layout = ({ pageTitle, children }) => {
-
   return (
     <div className={style.container}>
       <Header />
-      <main key={pageTitle}>
+      <main key={pageTitle} >
         <h1 className={style.heading}>{pageTitle}</h1>
         {children}
       </main>
